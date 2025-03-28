b71c1123d84607f6b47f9307739b88ecb9a12e05
FabricioSilvany <fabriciosilvany57@gmail.com>
2025-03-28 19:23:00 +0000

    Relógio completo

---
+++ js/script.js
@@ -0,0 +1,33 @@
+//dom
+
+const horas = document.querySelector('#horas')
+const minutos = document.querySelector('#minutos')
+const segundos = document.querySelector('#segundos')
+
+//eventos
+
+setInterval(relogio,1000)
+
+//funções
+
+function relogio(){
+    agora = new Date()
+
+    h = agora.getHours()
+    m = agora.getMinutes()
+    s = agora.getSeconds()
+
+    horas.textContent = h
+    minutos.textContent = m
+    segundos.textContent = s
+
+    if(h<10){
+        h = '0'+h
+    }
+    if(m<10){
+        m = '0'+m
+    }
+    if(s<10){
+        s = '0'+s
+    }
+}
